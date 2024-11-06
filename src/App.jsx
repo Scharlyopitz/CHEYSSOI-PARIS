761b8713d15eda813bd8da206bb718f69e2186bb
Scharly <scharly.opitz@gmail.com>
2024-11-06 14:45:16 +0100

    Ajout de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./style/style.css";
 import Home from "./pages/Home";
+import Nav from "./components/nav";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Nav() {
+  const links = ["About", "Team", "Services", "Contact"];
+
+  return (
+    <div>
+      {links.map((link, i) => {
+        return (
+          <NavLink key={i} to={`/${link}`}>
+            {link}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+}
