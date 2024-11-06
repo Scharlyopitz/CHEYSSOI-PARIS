5cd1ab9470f028fa6ac085f051bf643bf3a7cd7d
Scharly <scharly.opitz@gmail.com>
2024-11-06 14:48:51 +0100

    Changement des routes de la nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
-  const links = ["About", "Team", "Services", "Contact"];
+  const links = [
+    {
+      name: "About",
+      to: "/about",
+    },
+    {
+      name: "Team",
+      to: "/team",
+    },
+    {
+      name: "Services",
+      to: "/services",
+    },
+    {
+      name: "Contact",
+      to: "/contact",
+    },
+  ];
 
   return (
     <div>
       {links.map((link, i) => {
         return (
-          <NavLink key={i} to={`/${link}`}>
-            {link}
+          <NavLink key={i} to={link.to}>
+            {link.name}
           </NavLink>
         );
       })}
