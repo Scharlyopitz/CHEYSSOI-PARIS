1978c2190e7298bb12a9216ae518f3014cd1edca
Scharly <scharly.opitz@gmail.com>
2024-11-07 14:42:58 +0100

    Rename de la class

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,12 +4,18 @@ import Image1 from "/Image1.webp";
 export default function About() {
   return (
     <main id="About">
-      <section>
-        <div className="txt">
-          <h1>Cheyssoi Paris</h1>
-        </div>
-        <img src={Image1} alt="Image1" />
-      </section>
+      <FirstSection />
     </main>
+  );
+}
+
+function FirstSection() {
+  return (
+    <section>
+      <div className="txtContainer">
+        <h1>Cheyssoi Paris</h1>
+      </div>
+      <img src={Image1} alt="Image1" />
+    </section>
   );
 }
