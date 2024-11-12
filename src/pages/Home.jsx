a11ac807a746bfa7103341ebc1ad1f498a7c6147
Scharly <scharly.opitz@gmail.com>
2024-11-12 13:47:04 +0100

    Changement indentation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,24 @@ import Image1 from "/Image1.webp";
 export default function Home() {
   return (
     <main id="Home">
+      <Texte />
       <Image />
     </main>
   );
 }
 
+function Texte() {
+  return (
+    <div className="logo">
+      <p>Cheyssoi Paris</p>
+    </div>
+  );
+}
+
 function Image() {
   return (
-    <>
-      <div className="logo">
-        <p>Cheyssoi Paris</p>
-      </div>
-      <div className="image">
-        <img src={Image1} alt="Image1" />
-      </div>
-    </>
+    <div className="image">
+      <img src={Image1} alt="Image1" />
+    </div>
   );
 }
