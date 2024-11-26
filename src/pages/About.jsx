0ca14851008587a54af7a60d5b5d5a169e64c416
Scharly <scharly.opitz@gmail.com>
2024-11-26 13:26:57 +0100

    Ajout des liens pour le ebook

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,7 @@ import Observer from "../components/Observer";
 import { useState } from "react";
 import TextSeparation from "../components/Apropos/TextSeparation";
 import Formulaire from "../components/DemarrerMonProjet/Formulaire";
+import { NavLink } from "react-router-dom";
 
 export default function About() {
   const [activeSection, setActiveSection] = useState("homeSection");
@@ -127,5 +128,29 @@ function DemarrerMonProjet() {
 }
 
 function Ebook() {
-  return <section id="ebook"></section>;
+  const ebookLink = `https://www.amazon.fr/CREER-INTERIEUR-ECO-RESPONSABLE-ELEGANT-ebook/dp/B0D73LSF6C/ref=sr_1_1?
+
+__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=31XJVAMSA9CSQ&dib=eyJ2IjoiMSJ9.A4z4bMsJWqTjD5P2uzG2Pk6BHSfM10a6b8b
+
+VmdiYqxeEK-iTWVx-FosmOjkyjxlQtmgRclONO9W5RTWEiu7Ovs_Gubxw2-
+
+FPy3ZpSn2H1oX94jx3n7QfeDeEt_fThcwl2t0qMa6tvFN1P6QjMFG7Ja7tLMwRm5pUeIYKBayW15LpxCB6Wu_npvC9nrqGmE9nEDlUTawFbx3ue1mz
+cqx1wG_-3ywsVy2W5DKAo3ZXhTyPTe47YF9gnxfkqJg5oyp4EkpPHEq9Q2HccGUYdV2yu5oU4kazHDdD0WArpj3NBt0.8NQb37_3dkeeC5CROKbx
+5qpH0FHw4JAwZ78VHYd4GCg&dib_tag=se&keywords=CREER+UN+INTERIEUR+ECO+RESPONSABLE&nsdOptOutParam=true&qid=173195137
+
+4&sprefix=creer+un+interieur+eco+responsable%2Caps%2C137&sr=8-1`;
+
+  return (
+    <section id="ebook">
+      <h2>Notre Ebook</h2>
+      <p>
+        Créer un intérieur éco-responsable élégant" est un guide pour
+        transformer votre espace de vie en un sanctuaire chic et respectueux de
+        l'environnement.
+      </p>
+      <NavLink to={ebookLink} target="_blank">
+        Découvrir notre Ebook
+      </NavLink>
+    </section>
+  );
 }
