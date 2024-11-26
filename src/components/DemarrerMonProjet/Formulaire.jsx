f941318e3ed680ce388094cbcc20fe8b4117b41b
Scharly <scharly.opitz@gmail.com>
2024-11-26 13:10:54 +0100

    Ajout d'email Js pour l'envoie du formulaire

--- src/components/DemarrerMonProjet/Formulaire.jsx
+++ src/components/DemarrerMonProjet/Formulaire.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Formulaire() {
   const [informations, setInformations] = useState({
@@ -14,8 +15,8 @@ export default function Formulaire() {
   const [error, setError] = useState(false);
   const [messageOff, setmessageOff] = useState(false);
 
-  const SuccesMsg = `Message Sent`;
-  const ErrorMsg = "Complete all Fields";
+  const SuccesMsg = `Email envoyé`;
+  const ErrorMsg = "Veuillez remplir tout les champs";
 
   const clearFields = () => {
     setInformations({
@@ -35,194 +36,258 @@ export default function Formulaire() {
     }, 1500);
   };
 
-  //   const sendEmail = (e) => {
-  //     e.preventDefault();
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
 
-  //     const serviceId = "service_is6z0jh";
-  //     const templateId = "template_723rf7y";
-  //     const plublicKey = "Ivv9VH-K6JIKana-R";
+  //   const serviceId = "service_s2gudpp";
+  //   const templateId = "template_vj885kk";
+  //   const plublicKey = "Ivv9VH-K6JIKana-R";
 
-  //     const templateParams = {
-  //       user_message: informations.message,
-  //       user_email: informations.email,
-  //       user_name: informations.name,
-  //       to_name: "Wilhelm le boss",
-  //     };
-
-  //     const sendEmail = () => {
-  //       emailjs
-  //         .send(serviceId, templateId, templateParams, plublicKey)
-  //         .then((reponse) => {
-  //           if (
-  //             informations.surface !== "" &&
-  //             informations.message !== "" &&
-  //             informations.name !== "" &&
-  //             informations.firstname !== "" &&
-  //             informations.tel !== "" &&
-  //             informations.email !== "" &&
-  //             informations.postalCode !== ""
-  //           ) {
-  //             console.log(reponse);
-  //             clearFields();
-  //             setButtonMessage(SuccesMsg);
-  //             refreshButton();
-  //           } else {
-  //             setButtonMessage(ErrorMsg);
-  //             refreshButton();
-  //           }
-  //         })
-  //         .catch((err) => {
-  //           console.log(err);
-  //         });
-  //     };
-
-  //     if (
-  //       informations.surface !== "" &&
-  //       informations.message !== "" &&
-  //       informations.name !== "" &&
-  //       informations.firstname !== "" &&
-  //       informations.tel !== "" &&
-  //       informations.email !== "" &&
-  //       informations.postalCode !== ""
-  //     ) {
-  //       clearFields();
-  //       setError(false);
-  //       refreshMessage();
-  //       setmessageOff(true);
-  //       sendEmail();
-  //     } else {
-  //       setError(true);
-  //       setmessageOff(true);
-  //     }
+  //   const templateParams = {
+  //     name: informations.name,
+  //     firstname: informations.firstname,
+  //     tel: informations.tel,
+  //     email: informations.email,
+  //     surface: informations.surface,
+  //     postalCode: informations.postalCode,
+  //     message: informations.message,
   //   };
+
+  //   const sendEmail = () => {
+  //     emailjs
+  //       .send(serviceId, templateId, templateParams, plublicKey)
+  //       .then((reponse) => {
+  //         if (
+  //           informations.surface !== "" &&
+  //           informations.message !== "" &&
+  //           informations.name !== "" &&
+  //           informations.firstname !== "" &&
+  //           informations.tel !== "" &&
+  //           informations.email !== "" &&
+  //           informations.postalCode !== ""
+  //         ) {
+  //           console.log(reponse);
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
+
+  //   if (
+  //     informations.surface !== "" &&
+  //     informations.message !== "" &&
+  //     informations.name !== "" &&
+  //     informations.firstname !== "" &&
+  //     informations.tel !== "" &&
+  //     informations.email !== "" &&
+  //     informations.postalCode !== ""
+  //   ) {
+  //     clearFields();
+  //     setError(false);
+  //     refreshMessage();
+  //     setmessageOff(true);
+  //     sendEmail();
+  //   } else {
+  //     setError(true);
+  //     setmessageOff(true);
+  //   }
+  // };
 
   return (
     <div className="formulaire">
       <h2>Démarrer mon projet</h2>
 
       <form onSubmit={(e) => sendEmail(e)}>
+        <span
+          className="message"
+          style={{
+            opacity: messageOff ? 1 : 0,
+            background: error ? "#c642428c" : "#43d35f8c",
+          }}
+        >
+          {error ? ErrorMsg : SuccesMsg}
+        </span>
         <div className="middle">
-          <div className="left">
-            <div>
-              <label htmlFor="name">
-                Nom <span>*</span>{" "}
-              </label>
-              <input
-                autoComplete="off"
-                value={informations.name}
-                onChange={(e) =>
-                  setInformations((prec) => ({ ...prec, name: e.target.value }))
-                }
-                type="text"
-                id="name"
-              />
-            </div>
-            <div>
-              <label htmlFor="firstName">
-                Prénom <span>*</span>{" "}
-              </label>
-              <input
-                autoComplete="off"
-                value={informations.firstname}
-                onChange={(e) =>
-                  setInformations((prec) => ({
-                    ...prec,
-                    firstname: e.target.value,
-                  }))
-                }
-                type="text"
-                id="firstName"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="surface">
-                Surface <span>*</span>{" "}
-              </label>
-              <input
-                autoComplete="off"
-                value={informations.surface}
-                onChange={(e) =>
-                  setInformations((prec) => ({
-                    ...prec,
-                    surface: e.target.value,
-                  }))
-                }
-                type="text"
-                id="surface"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="tel">
-                Tel. <span>*</span>{" "}
-              </label>
-              <input
-                autoComplete="off"
-                value={informations.tel}
-                onChange={(e) =>
-                  setInformations((prec) => ({ ...prec, tel: e.target.value }))
-                }
-                type="text"
-                id="tel"
-              />
-            </div>
-            <div>
-              <label htmlFor="mail">
-                Email <span>*</span>
-              </label>
-              <input
-                autoComplete="off"
-                value={informations.email}
-                onChange={(e) =>
-                  setInformations((prec) => ({
-                    ...prec,
-                    email: e.target.value,
-                  }))
-                }
-                type="email"
-                id="mail"
-              />
-            </div>
-
-            <div>
-              <label htmlFor="postalCode">
-                Code Postal <span>*</span>{" "}
-              </label>
-              <input
-                autoComplete="off"
-                value={informations.postalCode}
-                onChange={(e) =>
-                  setInformations((prec) => ({
-                    ...prec,
-                    postalCode: e.target.value,
-                  }))
-                }
-                type="text"
-                id="postalCode"
-              />
-            </div>
-          </div>
-
-          <div className="right">
-            <textarea
-              autoComplete="off"
-              placeholder="Décrivez votre projet"
-              value={informations.message}
-              onChange={(e) =>
-                setInformations((prec) => ({
-                  ...prec,
-                  message: e.target.value,
-                }))
-              }
-              id="msg"
-            ></textarea>
-          </div>
+          <LeftPart
+            informations={informations}
+            setInformations={setInformations}
+          />
+          <RigthPart
+            informations={informations}
+            setInformations={setInformations}
+          />
         </div>
         <button type="submit">
           <p>Envoyer</p>
         </button>
       </form>
+    </div>
+  );
+}
+
+function LeftPart({ informations, setInformations }) {
+  return (
+    <div className="left">
+      <Nom informations={informations} setInformations={setInformations} />
+      <Prenom informations={informations} setInformations={setInformations} />
+      <Surface informations={informations} setInformations={setInformations} />
+      <Telephone
+        informations={informations}
+        setInformations={setInformations}
+      />
+      <Mail informations={informations} setInformations={setInformations} />
+      <CodePostal
+        informations={informations}
+        setInformations={setInformations}
+      />
+    </div>
+  );
+}
+
+function Nom({ informations, setInformations }) {
+  return (
+    <div>
+      <label htmlFor="name">
+        Nom <span>*</span>{" "}
+      </label>
+      <input
+        autoComplete="off"
+        value={informations.name}
+        onChange={(e) =>
+          setInformations((prec) => ({ ...prec, name: e.target.value }))
+        }
+        type="text"
+        id="name"
+      />
+    </div>
+  );
+}
+
+function Prenom({ informations, setInformations }) {
+  return (
+    <div>
+      <label htmlFor="firstName">
+        Prénom <span>*</span>{" "}
+      </label>
+      <input
+        autoComplete="off"
+        value={informations.firstname}
+        onChange={(e) =>
+          setInformations((prec) => ({
+            ...prec,
+            firstname: e.target.value,
+          }))
+        }
+        type="text"
+        id="firstName"
+      />
+    </div>
+  );
+}
+
+function Surface({ informations, setInformations }) {
+  return (
+    <div>
+      <label htmlFor="surface">
+        Surface (m²)<span>*</span>{" "}
+      </label>
+      <input
+        autoComplete="off"
+        value={informations.surface}
+        onChange={(e) =>
+          setInformations((prec) => ({
+            ...prec,
+            surface: e.target.value,
+          }))
+        }
+        type="text"
+        id="surface"
+      />
+    </div>
+  );
+}
+
+function Telephone({ informations, setInformations }) {
+  return (
+    <div>
+      <label htmlFor="tel">
+        Tel. <span>*</span>{" "}
+      </label>
+      <input
+        maxLength={10}
+        autoComplete="off"
+        value={informations.tel}
+        onChange={(e) =>
+          setInformations((prec) => ({ ...prec, tel: e.target.value }))
+        }
+        type="tel"
+        id="tel"
+      />
+    </div>
+  );
+}
+
+function Mail({ informations, setInformations }) {
+  return (
+    <div>
+      <label htmlFor="mail">
+        Email <span>*</span>
+      </label>
+      <input
+        autoComplete="off"
+        value={informations.email}
+        onChange={(e) =>
+          setInformations((prec) => ({
+            ...prec,
+            email: e.target.value,
+          }))
+        }
+        type="email"
+        id="mail"
+      />
+    </div>
+  );
+}
+
+function CodePostal({ informations, setInformations }) {
+  return (
+    <div>
+      <label htmlFor="postalCode">
+        Code Postal <span>*</span>{" "}
+      </label>
+      <input
+        maxLength={5}
+        autoComplete="off"
+        value={informations.postalCode}
+        onChange={(e) =>
+          setInformations((prec) => ({
+            ...prec,
+            postalCode: e.target.value,
+          }))
+        }
+        type="text"
+        id="postalCode"
+      />
+    </div>
+  );
+}
+
+function RigthPart({ informations, setInformations }) {
+  return (
+    <div className="right">
+      <textarea
+        autoComplete="off"
+        placeholder="Décrivez votre projet"
+        value={informations.message}
+        onChange={(e) =>
+          setInformations((prec) => ({
+            ...prec,
+            message: e.target.value,
+          }))
+        }
+        id="msg"
+      ></textarea>
     </div>
   );
 }
