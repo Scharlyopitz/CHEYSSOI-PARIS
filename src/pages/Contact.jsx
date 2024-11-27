e6960b5b2d5e1c37133fd83225592e27002ed6be
Scharly <scharly.opitz@gmail.com>
2024-11-27 10:48:18 +0100

    Création page contact

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Formulaire from "../components/DemarrerMonProjet/Formulaire";
+
+export default function Contact() {
+  return (
+    <section id="contact">
+      <Formulaire />
+    </section>
+  );
+}
