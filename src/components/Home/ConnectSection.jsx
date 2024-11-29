71c83121db7e5f88b6062324bf4767ef7d9dca73
Scharly <scharly.opitz@gmail.com>
2024-11-29 14:28:44 +0100

    Changement texte de la section

--- src/components/Home/ConnectSection.jsx
+++ src/components/Home/ConnectSection.jsx
@@ -3,7 +3,11 @@ import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function ConnectSection() {
-  const titleSection = "Connectons nous";
+  const titleSection = "UNE EXPERIENCE UNIQUE";
+
+  const txt =
+    "Cheyssoi est votre studio de design spécialisé dans les intérieurs éthiques et durables. Nous vous accompagnons dans la création de lieux uniques, où esthétisme et respect de l’environnement s’entrelacent harmonieusement. Cheyssoi Paris, c’est avant tout une expérience sur-mesure : inspirez-vous de nos idées et laissez-vous guider à chaque étape, jusqu’à la réalisation d’un espace en parfaite adéquation avec vos valeurs et vos envies. Nous mettons un point d’honneur à collaborer avec des artisans locaux, valorisant savoir-faire, circularité et matériaux durables, pour des créations qui font sens et durent dans le temps.";
+
   return (
     <section id="ConnextSection">
       <div className="titleSection">
@@ -11,12 +15,7 @@ export default function ConnectSection() {
           return <h2 key={i}>{word}</h2>;
         })}
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        incidunt delectus, perferendis rem maxime recusandae dicta ipsam minima
-        accusamus reiciendis dolorem, illo quia tempore quisquam odit laborum
-        voluptatibus fugit vel!
-      </p>
+      <p>{txt}</p>
       <NavLink to={"demarrermonprojet"}>
         <div className="button">
           <FontAwesomeIcon icon={faArrowRight} />
