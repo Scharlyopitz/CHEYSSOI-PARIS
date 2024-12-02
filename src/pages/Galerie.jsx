f7b18694aa2848075d2549ab942fb60c8c9f3586
Scharly <scharly.opitz@gmail.com>
2024-12-02 15:55:13 +0100

    Ajout du contenu de la galerie et son style

--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Galerie() {
-  return <main id="galerie"></main>;
+  const title = "L'art du detail";
+
+  const text =
+    "Plongez dans notre collection de projets d’intérieurs inspirants, où design et durabilité se rencontrent.";
+  return (
+    <main id="galerie">
+      <Navbar />
+      <div className="titleContainer">
+        {title.split(" ").map((word, i) => {
+          return <h2 key={i}>{word}</h2>;
+        })}
+        <p>{text}</p>
+      </div>
+    </main>
+  );
 }
