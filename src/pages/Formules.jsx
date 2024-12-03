36d5a924368b674f60b50859eab1fc75b7260ad4
Scharly <scharly.opitz@gmail.com>
2024-12-03 13:10:17 +0100

    Style des formules

--- src/pages/Formules.jsx
+++ src/pages/Formules.jsx
@@ -2,31 +2,52 @@ export default function Formules() {
   const title = "Nos Formules";
   return (
     <section id="formules">
-      <div className="title">
-        {title.split(" ").map((word, i) => {
-          return <h2 key={i}>{word}</h2>;
-        })}
+      <div className="contenu">
+        <div className="title">
+          {title.split(" ").map((word, i) => {
+            return <h2 key={i}>{word}</h2>;
+          })}
+        </div>
+
+        <div className="cardsContainer">
+          <FormuleCard
+            name={"Chey"}
+            price={"49"}
+            descriptions={[
+              "Conseils",
+              "Etude personnalisée",
+              "Accompagnement personnalisé",
+              "Planche d'ambiances",
+              "Plans 2D et Visuels 3D",
+              "Présentation des matériaux à utiliser",
+              "Shopping list",
+              "Coffret livré à domicile",
+              "Mise en lien avec artisans",
+            ]}
+          />
+        </div>
       </div>
-      <FormuleCard />
     </section>
   );
 }
 
-function FormuleCard() {
+function FormuleCard({ name, price, descriptions }) {
   return (
     <div className="formuleCard">
-      <div className="Name">Chey</div>
-      <div className="price">11€ / m²</div>
-
+      <div className="topContainer">
+        <div className="Name">{name}</div>
+        <div className="price">{price} €/m²</div>
+      </div>
       <div className="descriptif">
         <ul>
-          <li>Super</li>
-          <li>Super</li>
-          <li>Super</li>
-          <li>Super</li>
+          {descriptions.map((description, i) => {
+            return <li key={i}>{description}</li>;
+          })}
         </ul>
       </div>
-      <a href="#demarrerprojet">Commander</a>
+      <a href="#demarrerprojet" className="redButton">
+        Commander
+      </a>
     </div>
   );
 }
