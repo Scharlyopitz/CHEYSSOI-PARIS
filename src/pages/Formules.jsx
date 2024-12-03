6d2c9ccba115c3520d072bfe5b72a50993987ad4
Scharly <scharly.opitz@gmail.com>
2024-12-03 12:41:34 +0100

    Mise en place du style des formules

--- src/pages/Formules.jsx
+++ src/pages/Formules.jsx
@@ -1,3 +1,32 @@
 export default function Formules() {
-  return <section id="formules">LES FORMULES</section>;
+  const title = "Nos Formules";
+  return (
+    <section id="formules">
+      <div className="title">
+        {title.split(" ").map((word, i) => {
+          return <h2 key={i}>{word}</h2>;
+        })}
+      </div>
+      <FormuleCard />
+    </section>
+  );
+}
+
+function FormuleCard() {
+  return (
+    <div className="formuleCard">
+      <div className="Name">Chey</div>
+      <div className="price">11€ / m²</div>
+
+      <div className="descriptif">
+        <ul>
+          <li>Super</li>
+          <li>Super</li>
+          <li>Super</li>
+          <li>Super</li>
+        </ul>
+      </div>
+      <a href="#demarrerprojet">Commander</a>
+    </div>
+  );
 }
