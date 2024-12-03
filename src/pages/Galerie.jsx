1927b78f2f51320d8da5e64bd6366ed330f9e3df
Scharly <scharly.opitz@gmail.com>
2024-12-03 16:51:21 +0100

    Ajout des photo et du dossier json pour la data de chaques projets

--- src/pages/Galerie.jsx
+++ src/pages/Galerie.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import GalerieData from "../assets/GalerieData.json";
 
 export default function Galerie() {
   const title = "L'art du detail";
@@ -7,11 +7,28 @@ export default function Galerie() {
     "Plongez dans notre collection de projets d’intérieurs inspirants, où design et durabilité se rencontrent.";
   return (
     <section id="galerie">
-      <div className="titleContainer">
-        {title.split(" ").map((word, i) => {
-          return <h2 key={i}>{word}</h2>;
-        })}
-        <p>{text}</p>
+      <div className="content">
+        <div className="titleContainer">
+          <div className="h2Container">
+            {title.split(" ").map((word, i) => {
+              return <h2 key={i}>{word}</h2>;
+            })}
+          </div>
+          <p>{text}</p>
+        </div>
+
+        <div className="projectsContainer">
+          {GalerieData.map((galerie, i) => {
+            return (
+              <div key={i} className="projectContainer">
+                <div className="projectsNameContainer">
+                  <p>{galerie.name}</p>
+                </div>
+                <div className="project"></div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
