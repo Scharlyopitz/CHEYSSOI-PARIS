54a67ec26cfceb14d9258aba8b2e64c42ab3428b
Scharly <scharly.opitz@gmail.com>
2024-12-04 12:37:13 +0100

    Ajout de l'animation au scroll pour révéler le boutton

--- src/components/GotoTopButton.jsx
+++ src/components/GotoTopButton.jsx
@@ -1,11 +1,48 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+import { animate, motion as m } from "framer-motion";
+
 export default function GotoTopButton() {
+  const [reveal, setReveal] = useState(false);
+
+  const revealButton = {
+    initial: {
+      x: "101%",
+    },
+    animate: {
+      x: 0,
+    },
+  };
+
+  window.addEventListener("scroll", () => {
+    const { scrollTop } = document.documentElement;
+
+    const height = window.innerHeight;
+
+    if (scrollTop > height) {
+      setReveal(true);
+    } else {
+      setReveal(false);
+    }
+  });
+
   return (
-    <div className="gotoTopButton">
-      <a href="#" className="button">
+    <div
+      style={{ pointerEvents: reveal ? "all" : "none" }}
+      className="gotoTopButton"
+    >
+      <m.a
+        initial="initial"
+        animate={reveal ? "animate" : "initial"}
+        variants={revealButton}
+        transition={{ duration: 0.3, ease: [0.65, 0, 0.35, 1] }}
+        href="#"
+        className="button"
+      >
         <FontAwesomeIcon icon={faArrowUp} />
-      </a>
+      </m.a>
     </div>
   );
 }
