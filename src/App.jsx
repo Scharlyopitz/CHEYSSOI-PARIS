9014992b43455e238f5b55274427b1429a05afa6
Scharly <scharly.opitz@gmail.com>
2024-12-04 14:14:47 +0100

    Suppression des routes innutiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./style/style.css";
-import DemarrerProjet from "./pages/DemarrerProjet";
 import Home from "./pages/Home";
 import { useEffect } from "react";
-import Apropos from "./pages/Apropos";
-import Team from "./pages/Team";
-import Galerie from "./pages/Galerie";
 
 function App() {
   useEffect(() => {
@@ -15,10 +11,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/apropos" element={<Apropos />} />
-      <Route path="/demarrermonprojet" element={<DemarrerProjet />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="/galerie" element={<Galerie />} />
     </Routes>
   );
 }
