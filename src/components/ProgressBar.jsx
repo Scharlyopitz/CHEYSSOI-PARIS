dd4b9df7236cb38577a9d47ebe3c99dbb0fc0a50
Scharly <scharly.opitz@gmail.com>
2024-12-05 11:29:24 +0100

    Changement syntax

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,12 +2,12 @@ import { motion as m, useScroll, useTransform } from "framer-motion";
 
 export default function ProgressBar() {
   const { scrollYProgress } = useScroll();
-  const progressHeigth = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const height = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
   return (
     <div className="progressBarContainer">
       <div className="progressBar">
-        <m.div className="bar" style={{ height: progressHeigth }} />
+        <m.div className="bar" style={{ height }} />
       </div>
     </div>
   );
