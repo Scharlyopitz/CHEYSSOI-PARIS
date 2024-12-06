60fe3978c40f71559a3654c9ba4c008f987e2311
Scharly <scharly.opitz@gmail.com>
2024-12-06 10:56:24 +0100

    Mise au propre du code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-import Socials from "./Socials";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
@@ -17,6 +23,34 @@ function Contact() {
     <div className="contact">
       <div>+33 (0)7 81 16 56 22</div>
       <div>CHEYSSOIPARIS@GMAIL.COM</div>
+    </div>
+  );
+}
+
+function Socials() {
+  const socialMedias = [
+    {
+      icon: <FontAwesomeIcon icon={faFacebookF} />,
+      to: `https://www.facebook.com/CheyssoiParis/`,
+    },
+    {
+      icon: <FontAwesomeIcon icon={faInstagram} />,
+      to: `https://www.instagram.com/cheyssoiparis/`,
+    },
+    {
+      icon: <FontAwesomeIcon icon={faLinkedinIn} />,
+      to: `https://www.linkedin.com/in/tania-vingadassalon-design/`,
+    },
+  ];
+  return (
+    <div className="socials">
+      {socialMedias.map((social, i) => {
+        return (
+          <NavLink key={i} target="_blank" to={social.to}>
+            {social.icon}
+          </NavLink>
+        );
+      })}
     </div>
   );
 }
