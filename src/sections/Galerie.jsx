5ad3ed7328c25038240c9a6cfd916841f91a6ae2
Scharly <scharly.opitz@gmail.com>
2024-12-17 16:18:35 +0100

    Changement du designe de la section galerie

--- src/sections/Galerie.jsx
+++ src/sections/Galerie.jsx
@@ -19,24 +19,33 @@ export default function Galerie({ setProjectName }) {
 function Projects({ setProjectName }) {
   return (
     <div className="projectsContainer">
-      {GalerieData.map((galerie, i) => {
-        return (
-          <div
-            key={i}
-            onClick={() => setProjectName(galerie.name)}
-            className="projectContainer"
-          >
-            <div className="filter" />
-            <div className="project">
-              <img src={galerie.image} alt={galerie.name} />
-            </div>
-            <div className="projectsNameContainer">
-              <p>{galerie.name}</p>
-              <FontAwesomeIcon icon={faArrowRight} />
-            </div>
-          </div>
-        );
-      })}
+      <div className="left">
+        <div className="imageProjetContainer">
+          {GalerieData.map(({ image, name }, i) => {
+            return <img key={i} src={image} alt={name} />;
+          })}
+        </div>
+      </div>
+      <div className="right">
+        <div className="projectsNamesContainer">
+          {GalerieData.map((galerie, i) => {
+            return (
+              <div
+                key={i}
+                onClick={() => setProjectName(galerie.name)}
+                className="projectName"
+              >
+                <div className="hiddenContent">
+                  <span>
+                    {" "}
+                    <FontAwesomeIcon icon={faArrowRight} /> {galerie.name}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
