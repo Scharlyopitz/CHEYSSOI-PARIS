bc0f05be7189300d47c1978eeafc950c08a19d75
Scharly <scharly.opitz@gmail.com>
2024-12-18 11:47:09 +0100

    Ajout de la page espacepro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./style/style.css";
 import Home from "./pages/Home";
+import EspacePro from "./pages/EspacePro";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/espacepro" element={<EspacePro />} />
     </Routes>
   );
 }
---
+++ src/pages/EspacePro.jsx
@@ -0,0 +1,7 @@
+export default function EspacePro() {
+  return (
+    <main id="EspacePro">
+      <h1>espacepro</h1>
+    </main>
+  );
+}
