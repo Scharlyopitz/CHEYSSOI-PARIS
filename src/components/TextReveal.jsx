df0452149c2fc92cd534b9c173a9ffc5b452412f
Scharly <scharly.opitz@gmail.com>
2024-12-19 15:49:15 +0100

    Suppression de certaines animations

--- src/components/TextReveal.jsx
+++ src/components/TextReveal.jsx
@@ -1,21 +1,3 @@
-import { motion as m } from "framer-motion";
-import InView from "./InView";
-
 export default function TextReveal({ text }) {
-  const revealText = {
-    initial: {
-      background: "linear-gradient(90deg, #191a19 0%, #191a1929 0%)",
-    },
-    animate: {
-      background: "linear-gradient(90deg, #191a19 100%, #191a1929 100%)",
-      transition: { duration: 0.7, ease: [0.65, 0, 0.35, 1] },
-    },
-  };
-  return (
-    <InView>
-      <m.p className="Text" variants={revealText}>
-        {text}
-      </m.p>
-    </InView>
-  );
+  return <p className="Text">{text}</p>;
 }
--- src/components/TitleReveal.jsx
+++ src/components/TitleReveal.jsx
@@ -1,30 +1,13 @@
-import { motion as m } from "framer-motion";
-import InView from "./InView";
-
 export default function TitleReveal({ words }) {
-  const animeTitle = {
-    initial: {
-      y: "105%",
-    },
-    animate: (i) => ({
-      y: 0,
-      transition: { duration: 0.7, delay: i * 0.07, ease: [0.65, 0, 0.35, 1] },
-    }),
-  };
-
   return (
-    <InView>
-      <div className="titleContainer">
-        {words.map((word, i) => {
-          return (
-            <div key={i} className="wordContainer">
-              <m.h2 variants={animeTitle} custom={i}>
-                {word}
-              </m.h2>
-            </div>
-          );
-        })}
-      </div>
-    </InView>
+    <div className="titleContainer">
+      {words.map((word, i) => {
+        return (
+          <div key={i} className="wordContainer">
+            <h2>{word}</h2>
+          </div>
+        );
+      })}
+    </div>
   );
 }
