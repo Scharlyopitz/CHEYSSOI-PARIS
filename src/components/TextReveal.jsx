52e66ad7fe339441c7442421e45806301e8f8c8b
Scharly <scharly.opitz@gmail.com>
2024-12-19 12:13:43 +0100

    Changement code textReveal

--- src/components/TextReveal.jsx
+++ src/components/TextReveal.jsx
@@ -2,20 +2,18 @@ import { motion as m } from "framer-motion";
 import InView from "./InView";
 
 export default function TextReveal({ text }) {
+  const revealText = {
+    initial: {
+      background: "linear-gradient(90deg, #191a19 0%, #191a1929 0%)",
+    },
+    animate: {
+      background: "linear-gradient(90deg, #191a19 100%, #191a1929 100%)",
+      transition: { duration: 1, ease: [0.65, 0, 0.35, 1] },
+    },
+  };
   return (
     <InView>
-      <m.p
-        className="Text"
-        variants={{
-          initial: {
-            background: "linear-gradient(90deg, #191a19 0%, #191a1929 0%)",
-          },
-          animate: {
-            background: "linear-gradient(90deg, #191a19 100%, #191a1929 100%)",
-            transition: { duration: 1, ease: [0.65, 0, 0.35, 1] },
-          },
-        }}
-      >
+      <m.p className="Text" variants={revealText}>
         {text}
       </m.p>
     </InView>
